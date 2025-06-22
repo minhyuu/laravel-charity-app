01cd3bc001189ee5a75a0b4bf7493631e6e78e69
Danny <phamminhhieu129@gmail.com>
2025-06-22 22:32:12 +1000

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,13 @@ class AppServiceProvider extends ServiceProvider
         //
 
         // Force HTTPS in production environment
-        if(env('APP_ENV') === 'production') {
+        if (env('APP_ENV') === 'production') {
+            // Trust proxy headers for HTTPS
+            \Illuminate\Http\Request::setTrustedProxies(
+                [Request::getClientIp()],
+                \Illuminate\Http\Request::HEADER_X_FORWARDED_ALL
+            );
+
             URL::forceScheme('https');
         }
     }
