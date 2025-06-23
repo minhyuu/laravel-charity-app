f5c6fd1779d8c94aeee958c358a41fb0914f2037
Danny <phamminhhieu129@gmail.com>
2025-06-24 01:06:41 +1000

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -77,7 +77,7 @@
                                     </button>
 
                                     @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ secure_url('/password.request') }}">
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                     @endif
