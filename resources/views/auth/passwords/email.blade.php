09428a8054acbc40b4eee9a0c07511d86fc8b3d5
Danny <phamminhhieu129@gmail.com>
2025-06-25 21:29:21 +1000

    Update email.blade.php

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -15,7 +15,7 @@
                         </div>
                         @endif
 
-                        <form method="POST" action="{{ route('password.email') }}">
+                        <form method="POST" action="{{ secure_url('/password/email') }}">
                             @csrf
 
                             <div class="row mb-3">
